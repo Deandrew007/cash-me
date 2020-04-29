2d89c1c5cedcab21d28426acbc93c387db757c8e
Deandrew Moore <deamoore07@gmail.com>
2020-04-29 16:30:32 -0500

    started CashMe implementation by Tuwanie

---
+++ lib/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:hexcolor/hexcolor.dart';
+
+final Hexcolor primaryColor =  Hexcolor("#2B2D80");
+final Hexcolor secondaryColor =  Hexcolor("#F4981C");
+final Hexcolor whiteColor =  Hexcolor("#FFFFFF");
+final Hexcolor greyColor =  Hexcolor("#00000029");
+final Hexcolor redColor =  Hexcolor("#FF2A03");
+final Hexcolor greenColor =  Hexcolor("#3B802B");
+final Hexcolor lightergreenColor =  Hexcolor("#32D74B");
+
