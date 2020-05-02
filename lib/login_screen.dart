0606ebd0b4105cf2f17d73a3547c0ccfa075386a
Deandrew Moore <deamoore07@gmail.com>
2020-05-01 21:56:54 -0500

    working on button in login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,8 +9,83 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Hexcolor("#F4981C"),
+      body: Stack(children: <Widget>[
+        _buildlayout(),
+      ]),
     );
   }
+}
+
+Widget _buildlayout() {
+  return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          height: 325,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(
+              80,
+            )),
+            color: Hexcolor("#2B2D80"),
+          ),
+          child: Image.asset("assets/images/undraw_transfer_money.png"),
+        ),
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.only(top: 40, left: 25, right: 25),
+                  child: TextField(
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      hintText: "username",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide(
+                            color: Hexcolor("#2B2D80"),
+                            style: BorderStyle.solid,
+                          )),
+                    ),
+                  )),
+              Padding(
+                  padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      hintText: "password",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide(
+                            color: Hexcolor("#2B2D80"),
+                            style: BorderStyle.solid,
+                          )),
+                    ),
+                  )),
+              SizedBox(height: 40, width: 80,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Hexcolor("#2B2D80"))
+                ),
+                onPressed: () {},
+                textColor: Colors.white,
+                color: Hexcolor("#2B2D80"),
+                child: const Text('Sign In',
+                    style: TextStyle(fontSize: 20)),
+              ),
+              ),
+           
+            ],
+          ),
+        ),
+      ]);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Cash Me',
       theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
